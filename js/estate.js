498367ac92f27ba2f7803eb95bfa4ebbd8b84ce8
Owner <abcdot6661@gmail.com>
2022-09-24 13:13:56 +0500

    Changing file structure

--- js/estate.js
+++
@@ -1,7 +0,0 @@
-
-let heartSvgs = document.querySelectorAll('.heart-svg');
-heartSvgs.forEach((e)=>{
-        e.addEventListener('click',()=>{
-                e.classList.toggle('active')
-        })
-})
--- js/script.js
+++ js/script.js
@@ -26,6 +26,38 @@ $(document).ready(function () {
     $('.dark').click(function () {
         $('body').removeClass('dark-mode')
     })
+
+
+    // 
+
+    
+let heartSvgs = document.querySelectorAll('.heart-svg');
+heartSvgs.forEach((e)=>{
+        e.addEventListener('click',()=>{
+                e.classList.toggle('active')
+        })
+})
+
+// 
+    // Mouse Move Animation
+
+    document.querySelector('.hero-main').addEventListener('mousemove', (e) => {
+        let layerAni = document.querySelectorAll('.hero-ani svg');
+        layerAni.forEach((layer) => {
+            const speed = Math.floor((Math.random() * 10) + 1)
+            const x = (window.innerWidth - e.pageX * speed) / 120;
+            const y = (window.innerHeight - e.pageY * speed) / 120;
+            layer.style.transform = `translate(${x}px, ${y}px)`;
+        })
+    })
+    // 
+
+    $('.get-started').mousemove(function(e){
+        const x = (window.innerWidth - e.pageX)/20 ;
+            const y = (window.innerHeight*3 - e.pageY)/10;
+            $('.get-started').css('background-position', `${x}px ${y}px`)
+    })
+
     // 
 })
 
