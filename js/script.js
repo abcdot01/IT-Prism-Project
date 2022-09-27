43945b16176bb1a21b6b31702fa7e6f3aa49c881
Owner <abcdot6661@gmail.com>
2022-09-27 15:46:36 +0500

    Estate adding dropdown-menu or Some Fixing

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     // Footer Animation and date Setting
 
     const d = new Date();
@@ -30,44 +30,38 @@ $(document).ready(function () {
 
     // 
 
-    
-let heartSvgs = document.querySelectorAll('.heart-svg');
-heartSvgs.forEach((e)=>{
-        e.addEventListener('click',()=>{
-                e.classList.toggle('active')
+
+    let heartSvgs = document.querySelectorAll('.heart-svg');
+    heartSvgs.forEach((e) => {
+        e.addEventListener('click', () => {
+            e.classList.add('active')
         })
-})
+    })
 
 
 
-$(document).on('click', 'ul.tab-links li', function(){
-    $(this).addClass('active').siblings().removeClass('active')
-})
+    $(document).on('click', 'ul.tab-links li', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    })
 
 
-// 
+    // 
 
- $('.search-icon,.nav-close-btn').click(function(){
-    $('.navbar-form').toggleClass("show");
- })
+    $('.search-icon,.nav-close-btn,.noti-icon').click(function () {
+        $('.navbar-form').toggleClass("show");
+        $('.submenu').toggleClass("show");
+    })
 
-// 
+    // 
 
- $('.noti-icon').click(function(){
-    $('.submenu').toggleClass("show");
- })
-
-
-
-
-// 
+    // 
     // Mouse Move Animation
 
 
-    $('.get-started').mousemove(function(e){
-        const x = (window.innerWidth - e.pageX)/20 ;
-            const y = (window.innerHeight*3 - e.pageY)/10;
-            $('.get-started').css('background-position', `${x}px ${y}px`)
+    $('.get-started').mousemove(function (e) {
+        const x = (window.innerWidth - e.pageX) / 20;
+        const y = (window.innerHeight * 3 - e.pageY) / 10;
+        $('.get-started').css('background-position', `${x}px ${y}px`)
     })
 
 
@@ -77,15 +71,35 @@ $(document).on('click', 'ul.tab-links li', function(){
 // Mouse Move Animation
 
 const hero = document.querySelector('.hero-main');
+const twoDotMenu = document.querySelectorAll('.two-dot .svg');
 
-if(hero){
+if (hero) {
     hero.addEventListener('mousemove', (e) => {
-    let layerAni = document.querySelectorAll('.hero-ani svg');
-    layerAni.forEach((layer) => {
-        const speed = Math.floor((Math.random() * 10) + 1)
-        const x = (window.innerWidth - e.pageX * speed) / 120;
-        const y = (window.innerHeight - e.pageY * speed) / 120;
-        layer.style.transform = `translate(${x}px, ${y}px)`;
+        let layerAni = document.querySelectorAll('.hero-ani svg');
+        layerAni.forEach((layer) => {
+            const speed = Math.floor((Math.random() * 10) + 1)
+            const x = (window.innerWidth - e.pageX * speed) / 120;
+            const y = (window.innerHeight - e.pageY * speed) / 120;
+            layer.style.transform = `translate(${x}px, ${y}px)`;
+        })
     })
-})
+}
+
+if (twoDotMenu) {
+    twoDotMenu.forEach((e) => {
+        // 
+        e.addEventListener('click', () => {
+            // 
+            if (e.parentElement.classList.contains('show')) {
+                e.parentElement.classList.remove('show')
+            } else {
+                twoDotMenu.forEach(close => {
+                    close.parentElement.classList.remove('show')
+                    e.parentElement.classList.add('show')
+                })
+            }
+            // 
+        })
+        // 
+    })
 }
